152df1b7a8b8fd7acdd420790ef31d63e1e0d2d2
David MOLINARI <david@rmolinari.net>
2017-10-04 10:11:45 +0200

    function inline

--- test.m
+++ test.m
@@ -1,3 +1,13 @@
 ## fonction a la volee
 f='1./(1+x.^2)'
 fplot(f,[-2,6])
+
+## function anonym
+f=@(x)[1./(1+x.^2)]
+fplot(f,[-2,2])
+x=0;
+y=f(x)
+y=feval(f,x)
+x=[-2:0.5:2];
+y=f(x);
+plot(x,y,'-o')
