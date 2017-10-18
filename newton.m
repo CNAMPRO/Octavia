f904b7b727f37c0618f602fb3a3d298b584f3bff
David MOLINARI <david@rmolinari.net>
2017-10-18 11:56:52 +0200

    Newton V2

--- newton.m
+++ newton.m
@@ -7,10 +7,14 @@ function [y]=newton(P,x)
   for j=2:l
     A(j:l,j)=(A(j:l,j-1)-A(j-1:l-1,j-1))./(P(j:l,1)-P(1:l-j+1,1));
   end
-  Wi=ones(size(x));
-  y=A(1,1)*Wi;
-  for i=2:l
-    Wi=Wi.*(x-P(i-1,1));
-    y=y+A(i,i)*Wi;
+%  Wi=ones(size(x));
+%  y=A(1,1)*Wi;
+%  for i=2:l
+%    Wi=Wi.*(x-P(i-1,1));
+%    y=y+A(i,i)*Wi;
+%  end
+  y=zeros(size(x));
+  for k=l:-1:1
+    y=A(k,k)+(x-P(k,1)).*y;
   end
 end
