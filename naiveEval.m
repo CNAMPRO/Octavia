bd6d20b213a7fb5b1d0bad88c6ae4c1fdc0fdd97
David MOLINARI <david@rmolinari.net>
2017-10-18 10:26:38 +0200

    Decomposition fonction

---
+++ naiveEval.m
@@ -0,0 +1,7 @@
+## Fonction d'evaluation
+function [y]=naiveEval(alpha,x)
+  y = zeros(size(x));
+  for k=size(alpha):-1:1
+    y=alpha(k)+x.*y;
+  end
+end
--- naiveTest.m
+++ naiveTest.m
@@ -4,9 +4,3 @@ function [alpha]=naiveTest(P)
   V(:,2:l) = P(:,1).^(1:l-1);
   alpha = V\P(:,2);
 end
-
-
-
-
-
-  
