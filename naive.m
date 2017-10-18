1ecb8cd98e2686b52d044e11aeed2843391f0f42
David MOLINARI <david@rmolinari.net>
2017-10-18 10:20:22 +0200

    Function naive.m

---
+++ naive.m
@@ -0,0 +1,22 @@
+#Methode d'interpolatio naive
+#function [y]=naive(P,x)
+function [y]=naive(P,x)
+  [l,c]=size(P);
+  V = ones(l,l);
+  V(:,2:l) = P(:,1).^(1:l-1); 
+  alpha = V\P(:,2);
+  y=zeros(size(x));
+  for i=1:l
+    y+=alpha(i)*x.^(i-1);
+  end
+end
+  
+%  [l, c] = size(P);
+%  matrice = vander(P(1,:));
+
+
+
+
+
+
+  
