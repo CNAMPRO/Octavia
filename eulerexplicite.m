780e2e1667f5d758895a9a51bf4f970d4a57cc9f
David MOLINARI <david@rmolinari.net>
2017-10-25 12:27:28 +0200

    simplifi explicite

--- eulerexplicite.m
+++ eulerexplicite.m
@@ -6,7 +6,7 @@ function [u]=eulerexplicite(t0, T, y0, N, phi)
   h=(T-t0)/N;
   t=linspace(t0,T,N);
   for n=1:N-1
-    phin=feval(phi, t(n), u(n));
-    u(n+1) = u(n)+h*phin;
+    %phin=feval(phi, t(n), u(n));
+    u(n+1) = u(n)+h*phi(t(n), u(n));
   end
 end
