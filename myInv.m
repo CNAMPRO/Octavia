a9a2d30edc0d523b21f54594611851612111f597
David MOLINARI <david@rmolinari.net>
2017-12-06 12:08:07 +0100

    add myInv

--- myInv.m
+++ myInv.m
@@ -1,8 +1,10 @@
 function invA=myInv(A)
   [n,m]=size(A);
+  [L,U] = mylyImprov(A);
   for j=1:n
     b=zeros(n);
     b(j)=1;
-    invA(:,j)=syslin(A,b);
+    y=descente(L,b)';
+    invA(:,j)=remonteee(U,y)';
   end
 end
